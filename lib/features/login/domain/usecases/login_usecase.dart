e7392135e39a74ec67931d82b1f5fad9d32d43a7
cesar <106979505+CesarAugusto200@users.noreply.github.com>
2025-07-14 13:49:09 -0600

    implementacion

--- lib/features/login/domain/usecases/login_usecase.dart
+++ lib/features/login/domain/usecases/login_usecase.dart
@@ -1,11 +1,13 @@
 import 'package:dartz/dartz.dart';
-import '../repositories/auth_repository.dart';
+import 'package:untitled/features/login/domain/repositories/auth_repository.dart';
 import '../../../../core/error/failure.dart';
+import 'package:untitled/features/login/domain/repositories/auth_repository.dart';
+
 
 class LoginUseCase {
   final AuthRepository repository;
 
-  LoginUseCase(this.repository);
+  const LoginUseCase(this.repository);
 
   Future<Either<Failure, void>> call(String email, String password) {
     return repository.login(email, password);
